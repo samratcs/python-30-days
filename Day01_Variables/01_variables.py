2aebf1d27868aa94ccdddd59a1951acf941154a1
Samrat Sur <samrat.cs@outlook.com>
2026-07-11 00:00:14 +0530

    feat(day01): solve Question 1 - Student Information

---
+++ Day01_Variables/01_variables.py
@@ -0,0 +1,66 @@
+# =====================================================
+# Question 1
+# Student Information
+# =====================================================
+
+# Student Information
+
+student_name = "Samrat Sur"
+age = 24
+city = "Kolkata"
+course = "Web Development"
+percentage=80.0
+
+print("\tStudent Details\t")
+print("-"*30)
+print("Student Name         : ",student_name)
+print("Student Age          : ",age)
+print("Student City         : ",city)
+print("Student Course       : ",course)
+print("Student Percentage   : ",percentage,"%")
+
+
+
+print("-" * 50)
+
+
+# =====================================================
+# Question 2
+# Rectangle Dimensions
+# =====================================================
+
+
+
+
+
+print("-" * 50)
+
+
+# =====================================================
+# Question 3
+# Employee Information
+# =====================================================
+
+# Write your solution here
+
+
+print("-" * 50)
+
+
+# =====================================================
+# Question 4
+# Swap Two Variables
+# =====================================================
+
+# Write your solution here
+
+
+print("-" * 50)
+
+
+# =====================================================
+# Question 5
+# Mobile Phone Specifications
+# =====================================================
+
+# Write your solution here
