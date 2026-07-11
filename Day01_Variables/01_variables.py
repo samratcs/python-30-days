b3c2d3ab0d107e259c17a53f194878a3599b5cca
Samrat Sur <samrat.cs@outlook.com>
2026-07-11 08:57:36 +0530

    feat(day01): solve Question 2 - Rectangle Dimensions

--- Day01_Variables/01_variables.py
+++ Day01_Variables/01_variables.py
@@ -5,14 +5,19 @@
 
 # Student Information
 
+# Variable stores student's name
 student_name = "Samrat Sur"
+# Variable stores student's age
 age = 24
+# Variable stores student's city
 city = "Kolkata"
+# Variable stores student's course
 course = "Web Development"
+# Variable stores student's percentage
 percentage=80.0
 
-print("\tStudent Details\t")
-print("-"*30)
+print("="*16,"Student Details","="*16)
+
 print("Student Name         : ",student_name)
 print("Student Age          : ",age)
 print("Student City         : ",city)
@@ -29,8 +34,23 @@ print("-" * 50)
 # Rectangle Dimensions
 # =====================================================
 
+# Storing the length of rectangle
+length=20
+# storing the breadth of rectangle
+breadth=15
 
+#print a blanlk line
+print() 
+#display Information
+print("="*10,"Rectangle Dimensions","="*10)
+print("Length       :",length)
+print("Breadth      :",breadth)
 
+#find the area and perimeter
+area=length*breadth
+perimeter=2*(length+breadth)
+print("Area is      :",area)
+print("Perimeter is :",perimeter)
 
 
 print("-" * 50)
