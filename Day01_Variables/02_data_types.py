1eba51c5e3216ddef26139a03e64b2926b5a2fb1
Samrat Sur <samrat.cs@outlook.com>
2026-07-12 19:33:37 +0530

    feat(day01): solve Question 1 - Basic Data Types

---
+++ Day01_Variables/02_data_types.py
@@ -0,0 +1,45 @@
+# =====================================================
+# Question 1
+# Identify Python Data Types
+# =====================================================
+
+print("="*15,"DATA TYPES", "="*15)
+
+print()
+
+# Variable to store Integer Value
+age = 24
+
+# Variable to store Float Value
+height = 6.2
+
+# Variable to store String Value
+name = "Samrat"
+
+# Variable to store Boolean Value
+isStudent = True
+
+# Variable to store Complex Value
+complex_num = 2+6j
+
+print("Age                  :",age)
+print("Data Type            :",type(age))
+print()
+
+print("Height               :",height)
+print("Data Type            :",type(height))
+print()
+
+print("Name                 :",name)
+print("Data Type            :",type(name))
+print()
+
+print("Student              :",isStudent)
+print("Data Type            :",type(isStudent))
+print()
+
+print("Complex Number       :",complex_num)
+print("Data Type            :",type(complex_num))
+print()
+
+print("-"*50)
