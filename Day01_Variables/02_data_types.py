089cc063a377d58942ccf108b69eae4f4f0890c4
Samrat Sur <samrat.cs@outlook.com>
2026-07-12 23:10:46 +0530

    feat(day01): solve Question 2 - Collection Data Types

--- Day01_Variables/02_data_types.py
+++ Day01_Variables/02_data_types.py
@@ -43,3 +43,54 @@ print("Data Type            :",type(complex_num))
 print()
 
 print("-"*50)
+
+
+# =====================================================
+# Question 2
+# Student Skills (Collection Data Types)
+# =====================================================
+
+print("="*15,"Student Skills (Collection Data Types)", "="*15)
+
+print()
+
+# Creating List Data Structure to store five programming languages
+program_language = ["Python", "Java", "Dart", "C++", "C"]
+
+# Creating Tuple Data Structure to store five database names
+databases = ("MongoDB", "MySQL", "SQL Server", "IBM DB2", "Postress SQL")
+
+# Creating Set Data Structure to store software development tools
+software_tools = {"PyCharm", "IntelliJ Idea", "VS Code", "Postman", "WebStrom"}
+
+# Creating Dictionary Data Structure to store student information.
+student_info = {
+    "name":"Samrat Sur",
+    "Roll": "3434-61-0007",
+    "age": 20,
+    "course": "Computer Science",
+    "city": "Howrah",
+    "percentage": 65.50
+}
+
+print("Five Programming Languages:")
+print(program_language)
+print(type(program_language))
+print()
+
+print("Five Databases:")
+print(databases)
+print(type(databases))
+print()
+
+print("Five Software Tools:")
+print(software_tools)
+print(type(software_tools))
+print()
+
+print("Student Information:")
+print(student_info)
+print(type(student_info))
+print()
+
+print("-"*50)
