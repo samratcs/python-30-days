0ddacdd45a62a4c9cb9fb646ae6d54d9708a35a1
Samrat Sur <samrat.cs@outlook.com>
2026-07-14 23:57:15 +0530

    feat(day01): solve Question 1 - Student Registration

---
+++ Day01_Variables/03_input_output.py
@@ -0,0 +1,23 @@
+# =====================================================
+# Question 1
+# Student Registration Form
+# =====================================================
+
+print("="*15,"Personal Portfolio Data", "="*15)
+
+print()
+
+Student_Name = input("Enter Student Name")
+Age = int(input("Enter Student Age"))
+Gender = input("Enter Student Gender")
+Email = input("Enter Student Email")
+Phone_Number = int(input("Enter Student Phone No"))
+City = input("Enter Student Age")
+College_Name = input("Enter Student College Name")
+Course = input("Enter Student Course")
+
+
+
+print()
+
+print("-"*50)
