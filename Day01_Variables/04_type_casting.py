21757800d9f57c74525e0db08f0d2fba95f5fc48
Samrat Sur <samrat.cs@outlook.com>
2026-07-23 00:27:44 +0530

    feat(day01): solve Question 1 - Student Age Converterr

---
+++ Day01_Variables/04_type_casting.py
@@ -0,0 +1,38 @@
+# =====================================================
+# Question 1
+# Student Age Converter
+# =====================================================
+
+print("="*15,"Student Age Converterr", "="*15)
+
+print()
+
+
+# Takes the student's name. 
+Full_Name = input("Enter Student Name: ")
+# Takes the student's Age.
+Age = input("Enter Age: ")
+
+
+# Print the data type of the age before conversion.
+print("Before Type Conversion Type of Student's Age is :",type(Age))
+Designation = input("Enter Your Current Designation: ")
+
+# Convert the age to an integer.
+Age = int(Age)
+
+# Print the data type after conversion.
+print("After Type Conversion Type of Student's Age is :",type(Age))
+
+
+Age = Age + 5
+
+# Display the student's age after 5 years.
+print("After % years Student's Age is :",Age)
+
+
+
+print()
+
+print("-"*50)
+
