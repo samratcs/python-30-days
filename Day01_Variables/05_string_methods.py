101a318fa917a46ca5759214715dccc51a18dbfa
Samrat Sur <samrat.cs@outlook.com>
2026-07-25 22:27:46 +0530

    feat(day01): solve Question 1 - Personal Information Formatter

---
+++ Day01_Variables/05_string_methods.py
@@ -0,0 +1,37 @@
+# =====================================================
+# Question 1
+# Personal Information Formatter
+# =====================================================
+
+print("="*15,"Personal Information Formatter", "="*15)
+
+print()
+
+# Take the following input from the user:
+
+Full_Name = input("Enter Your Full Name")
+City = input("Enter Your City Name")
+Profession = input("Enter Your Profession")
+
+# Display:
+
+print("================ PERSONAL DETAILS ================")
+
+print("Original Name    :",Full_Name)
+print("Uppercase        :",Full_Name.upper())
+print("Lowercase        :",Full_Name.lower())
+print("Title Case       :",Full_Name.title())
+print("Length           :",len(Full_Name))
+
+print("==================================================")
+
+print()
+
+# =====================================================
+# Question 2
+# Email Validator
+# =====================================================
+
+print("="*15,"Email Validator", "="*15)
+
+print()
