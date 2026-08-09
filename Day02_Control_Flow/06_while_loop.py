b2a85427e77b51efbc058750e65b758487370e76
Samrat Sur <samrat.cs@outlook.com>
2026-08-09 16:29:52 +0530

    Day 2: Add number reversal using while loop

---
+++ Day02_Control_Flow/06_while_loop.py
@@ -0,0 +1,19 @@
+"""
+Question:
+Write a Python program that accepts an integer and reverses
+the number using a while loop.
+
+Example:
+Input: 12345
+Output: 54321
+"""
+n = int(input("Enter the value of n to find Reverse").strip())
+temp = n #store the valuer of n in temp
+reverse = 0
+while temp > 0:
+    rem = temp % 10
+    rev = rev*10 + rem
+
+print("Reverse is",rev)
+
+print("="*30)
