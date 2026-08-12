a5ee5d3cedb0c5ce67bfa074c93c8e10b25e2882
Samrat Sur <samrat.cs@outlook.com>
2026-08-12 23:30:56 +0530

    Day 3: Add function basics

---
+++ Day03_Functions/01_function_basics.py
@@ -0,0 +1,15 @@
+"""
+Question:
+Create a function called greet() that prints
+"Hello, Welcome to Python!"
+
+Call the function three times.
+"""
+
+def greet():
+    print("Hello")
+
+
+greet()
+greet()
+
