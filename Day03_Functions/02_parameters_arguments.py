4e9c23007f744f6d3e5beb42278279cbc5ab4ba1
Samrat Sur <samrat.cs@outlook.com>
2026-08-13 23:24:29 +0530

    Day 3: Add function parameters and arguments

---
+++ Day03_Functions/02_parameters_arguments.py
@@ -0,0 +1,16 @@
+"""
+Question:
+Create a function called greet_user(name) that accepts
+a user's name and prints a welcome message.
+
+Example:
+Input: Samrat
+Output: Hello Samrat, Welcome to Python!
+"""
+
+def greet(name):
+    print(f"Hello {name}, Welcome to Python!")
+
+
+name=input("Enter Your Name")
+greet(name)
