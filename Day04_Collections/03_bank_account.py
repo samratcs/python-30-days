1c10bd1438eb4ef00f2ac2285586d8cfc5c288c1
Samrat Sur <samrat.cs@outlook.com>
2026-08-21 23:27:05 +0530

    feat(day04) ass 03_bank_account.py modified

--- Day04_Collections/03_bank_account.py
+++ Day04_Collections/03_bank_account.py
@@ -27,5 +27,28 @@ Create a simple menu-driven banking system:
 3. Check Balance
 4. Exit
 
-
 """
+# initialize dictionary
+data={}
+# functions as follows
+
+
+
+# ask user to enter product names to add in the cart
+print("="*15,"Welcome to XYZ Bank","="*15)
+print("1. Deposit")
+print("2. Withdraw")
+print("3. Check Balance")
+print("4. Exit")
+
+while True:
+    choice = int(input("Enter Your Chices: ").strip())
+    match choice:
+        case 1:deposit()
+        case 2:withdraw()
+        case 3:getBalance()
+        case 4:exit(0)
+        case _:print("Invalid Option")
+
+
+print("="*30)
