bd4b6d82eb18f9f461ceb0bd00d566ce3256ba35
Samrat Sur <samrat.cs@outlook.com>
2026-08-22 16:37:39 +0530

    feat(day04) add 03_bank_account.py solution

--- Day04_Collections/03_bank_account.py
+++ Day04_Collections/03_bank_account.py
@@ -28,32 +28,100 @@ Create a simple menu-driven banking system:
 4. Exit
 
 """
+
+import random
+import datetime
+
 # initialize dictionary
-data={}
+accounts=[]
+
 # functions as follows
 
+def createAccount():
+    name=input("Enter Account Holder name: ")
+    mobile=input("Enter Your Mobile: ")
+    account_number="XYZ_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    data={}
+    data["name"]=name
+    data["account_number"]=account_number
+    data["mobile"]=mobile
+    data["balance"]=0.0
+    print("Your account opened successfully")
+    print("-"*30)
+    print("Account Holder:",data["name"])
+    print("Account Number:",data["account_number"])
+    print("Your Current Balance is:",data["balance"])
+    accounts.append(data)
+    return
+
 def deposit():
-    return
+    account_number = input("Enter Account Number: ")
+
+    for account in accounts:
+        if account["account_number"] == account_number:
+            amount = float(input("Enter Deposit Amount: "))
+
+            if amount > 0:
+                account["balance"] += amount
+                print("Amount deposited successfully!")
+                print("Current Balance:", account["balance"])
+            else:
+                print("Deposit amount must be greater than 0.")
+            return
+    else:
+        print("Account not found.")
+
 def withdraw():
-    return
-def getBalanc():
-    return
+    account_number = input("Enter Account Number: ")
+    
+    for account in accounts:
+        if account["account_number"] == account_number:
+            amount = float(input("Enter Withdraw Amount: "))
+    
+            if account["balance"]-amount > 0:
+                account["balance"] -= amount
+                print("Amount deposited successfully!")
+                print("Current Balance:", account["balance"])
+            else:
+                print("You dont Have sufficient amount of money in your account.")
+            return
+    else:
+        print("Account not found.")
+
+def getBalance():
+    account_number = input("Enter Account Number: ")
+        
+    for account in accounts:
+        if account["account_number"] == account_number:
+            print("\n")
+            print("Your Account Information".center(30))
+            print("Account Holder:",account["name"])
+            print("Account Number:",account["account_number"])
+            print("Your Current Balance is:",account["balance"])
+            print("\n")
+            print("="*50)
+            return
+    else:
+        print("Account Not Found")
+
 
 
 # ask user to enter product names to add in the cart
 print("="*15,"Welcome to XYZ Bank","="*15)
-print("1. Deposit")
-print("2. Withdraw")
-print("3. Check Balance")
-print("4. Exit")
+print("1. Account Opening")
+print("2. Deposit")
+print("3. Withdraw")
+print("4. Check Balance")
+print("5. Exit")
 
 while True:
     choice = int(input("Enter Your Chices: ").strip())
     match choice:
-        case 1:deposit()
-        case 2:withdraw()
-        case 3:getBalance()
-        case 4:exit(0)
+        case 1:createAccount()
+        case 2:deposit()
+        case 3:withdraw()
+        case 4:getBalance()
+        case 5:exit(0)
         case _:print("Invalid Option")
 
 
